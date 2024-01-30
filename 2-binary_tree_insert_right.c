1b06ff0d74c82bf089519e5a45dcc183c66c7e13
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-01-30 14:27:24 +0300

    fix adding right child

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,21 +14,20 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *node;
+	binary_tree_t *node;
 
-node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-if (!node || !parent)
-	return (NULL);
-node->n = value;
-node->parent = parent;
-node->left = NULL;
-if (!parent->right)
-{
+	node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (!node || !parent)
+		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	if (parent->right)
+	{
+		node->right = parent->right;
+		node->right->parent = node;
+	}
 	parent->right = node;
 	return (node);
-}
-node->right = parent->right;
-node->right->parent = node;
-parent->right = node;
-return (NULL);
 }
