444f0e02af4ee966aec94b6c6ca474a5c60ea2e3
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-01-30 14:38:48 +0300

    fix return (NULL);

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,8 +13,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (!parent)
+		return (NULL);
 	node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	if (!node || !parent)
+	if (!node)
 		return (NULL);
 	node->n = value;
 	node->parent = parent;
