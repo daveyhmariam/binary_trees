46749625c2658aa99b222dd483bb557ff34b319b
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-01-31 12:36:43 +0300

    fix returns and code based on checker

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,12 +26,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 int determine_height(const binary_tree_t *tree)
 {
-	int l, r;
+	int l = 0, r = 0;
 
 	if (tree)
 	{
-		l = tree->left ? 1 + determine_height(tree->left) : 0;
-		r = tree->right ? 1 + determine_height(tree->right) : 0;
+		l = tree->left ? 1 + determine_height(tree->left) : 1;
+		r = tree->right ? 1 + determine_height(tree->right) : 1;
 	}
 	return ((l > r) ? l : r);
 }
